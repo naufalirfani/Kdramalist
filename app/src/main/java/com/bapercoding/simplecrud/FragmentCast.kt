31ab9a7678b0261b35235095201e265be29e9ff2
naufalirfani <naufal.irfani3@gmail.com>
2020-09-03 10:54:06 +0700

    update

---
+++ app/src/main/java/com/bapercoding/simplecrud/FragmentCast.kt
@@ -0,0 +1,35 @@
+package com.bapercoding.simplecrud
+
+
+import android.os.Bundle
+import android.support.annotation.Nullable
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class FragmentCast : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cast, container, false)
+    }
+
+    override fun onViewCreated(
+            view: View,
+            @Nullable savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listPhoto = ArrayList<Photo>()
+        listPhoto.add(Photo(R.drawable.comingsoon))
+        rvDetail.setHasFixedSize(true)
+        rvDetail.layoutManager = LinearLayoutManager(context)
+        val adapter = context?.let { PhotoFilmAdapter(it,listPhoto) }
+        adapter?.notifyDataSetChanged()
+        rvDetail.adapter = adapter
+    }
+}
