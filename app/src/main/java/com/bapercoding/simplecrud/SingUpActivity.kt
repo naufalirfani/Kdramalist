e72ee6698e32f8ad47157faeb76d5598dd6bc15a
naufalirfani <naufal.irfani3@gmail.com>
2020-09-08 10:16:39 +0700

    update

---
+++ app/src/main/java/com/bapercoding/simplecrud/SingUpActivity.kt
@@ -0,0 +1,12 @@
+package com.bapercoding.simplecrud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SingUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+    }
+}
