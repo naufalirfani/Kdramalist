f7d94a9fea552438c123a99c7390b60e1bd59f60
naufalirfani <naufal.irfani3@gmail.com>
2020-09-09 11:22:46 +0700

    update

--- app/src/main/java/com/bapercoding/simplecrud/SingUpActivity.kt
+++ app/src/main/java/com/bapercoding/simplecrud/SingUpActivity.kt
@@ -23,6 +23,7 @@ class SingUpActivity : AppCompatActivity() {
     private lateinit var usernameEt: EditText
     private lateinit var emailEt: EditText
     private lateinit var passwordEt: EditText
+    private lateinit var repasswordEt: EditText
 
     private lateinit var signUpBtn: Button
 
@@ -40,18 +41,35 @@ class SingUpActivity : AppCompatActivity() {
         usernameEt = findViewById(R.id.et_username_sign)
         emailEt = findViewById(R.id.et_email_sign)
         passwordEt = findViewById(R.id.et_pass_sign)
+        repasswordEt = findViewById(R.id.et_reenterpass_sign)
         signUpBtn = findViewById(R.id.btn_signUp)
         val tvcreate: TextView = findViewById(R.id.tv_create)
+        val tvHave: TextView = findViewById(R.id.tv_have)
+        val tvLogin: TextView = findViewById(R.id.tv_Login)
+        tvHave.text = resources.getString(R.string.have_an_account)
+        tvLogin.text = resources.getString(R.string.login)
         tvcreate.setText(getString(R.string.create_an_account))
+
+        tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left)
+            finish()
+        }
 
         signUpBtn.setOnClickListener{
             var username: String = usernameEt.text.toString()
             var email: String = emailEt.text.toString()
             var password: String = passwordEt.text.toString()
+            var repassword: String = repasswordEt.text.toString()
 
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username)) {
+            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(username) || TextUtils.isEmpty(repassword)) {
                 Toast.makeText(this, "Please fill all the fields", Toast.LENGTH_LONG).show()
-            } else{
+            }
+            else if(password != repassword){
+                Toast.makeText(this, "Incorrect password", Toast.LENGTH_LONG).show()
+            }
+            else{
                 var user: UserInfo? = null
                 dbReference.child(username).addValueEventListener(object : ValueEventListener {
                     override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -66,18 +84,18 @@ class SingUpActivity : AppCompatActivity() {
                     createUser(username, email)
                     auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
                         if(task.isSuccessful){
-                            Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "Successfully registered", Toast.LENGTH_LONG).show()
                             val intent = Intent(this, LoginActivity::class.java)
-                            overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left)
                             startActivity(intent)
+                            overridePendingTransition(R.anim.left_to_right, R.anim.right_to_left)
                             finish()
                         }else {
-                            Toast.makeText(this, "Username or Email Already Exist", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "Username or rmail already used", Toast.LENGTH_LONG).show()
                         }
                     })
                 }
                 else{
-                    Toast.makeText(this, "Username or Email Already Exist", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Username or rmail already used", Toast.LENGTH_LONG).show()
                 }
             }
         }
