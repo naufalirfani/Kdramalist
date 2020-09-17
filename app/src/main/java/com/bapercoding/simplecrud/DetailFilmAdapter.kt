8805b4458754b0d912c9ff90335193c10b00e8d4
naufalirfani <naufal.irfani3@gmail.com>
2020-09-17 13:58:28 +0700

    update

--- app/src/main/java/com/bapercoding/simplecrud/DetailFilmAdapter.kt
+++ app/src/main/java/com/bapercoding/simplecrud/DetailFilmAdapter.kt
@@ -1,18 +1,18 @@
 package com.bapercoding.simplecrud
 
 import android.content.Context
-import android.content.Intent
 import android.support.v7.widget.RecyclerView
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RatingBar
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DecodeFormat
-import com.bumptech.glide.request.RequestOptions
-import com.bumptech.glide.request.target.Target
+import com.iarcuschin.simpleratingbar.SimpleRatingBar
 import kotlinx.android.synthetic.main.detail_film.view.*
+import java.text.DecimalFormat
 
+
+@Suppress("DEPRECATION")
 class DetailFilmAdapter(private val context: Context, private val listFilm: ArrayList<Film>, private val judul: String, private val rating: String, private val episode: String, private val sinopsis: String, private val imagePage: String, private val letak: Int) : RecyclerView.Adapter<DetailFilmAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
@@ -28,15 +28,16 @@ class DetailFilmAdapter(private val context: Context, private val listFilm: Arra
 //                .apply(RequestOptions().fitCenter().format(DecodeFormat.PREFER_ARGB_8888).override(Target.SIZE_ORIGINAL))
                 .into(holder.view.img_item_photo2)
 
-        holder.view.ratingbar.setOnRatingBarChangeListener(object : RatingBar.OnRatingBarChangeListener {
-            override fun onRatingChanged(p0: RatingBar?, p1: Float, p2: Boolean) {
-                holder.view.tv_rating_user.text = (p1*2).toString()
+        holder.view.ratingBar1.setOnRatingBarChangeListener(object : SimpleRatingBar.OnRatingBarChangeListener {
+            override fun onRatingChanged(simpleRatingBar: SimpleRatingBar?, rating: Float, fromUser: Boolean) {
+                val df = DecimalFormat("#.#")
+                holder.view.tv_rating_user.text = df.format(rating*2) + "/10"
             }
         })
-
+        holder.view.tv_rating_user.text = "0/10"
         holder.view.tv_rating.text = rating
         holder.view.tv_item_rating2.text = "Your rating:"
-        holder.view.tv_rating_all.text = "Ratings: ${rating} from users"
+        holder.view.tv_rating_all.text = Html.fromHtml("Ratings: " + "<b>" + rating + "</b>" + " from users")
         holder.view.tv_view_user.text = "Views:"
         holder.view.tv_item_rating.text = film.detail
         holder.view.tv_sinopsis.text = sinopsis
